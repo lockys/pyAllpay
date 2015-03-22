bbb3394d87e2aea4ad17cb9f5e0b85baa39935f6
lockys <locky4567@gmail.com>
2015-03-23 00:04:02 +0800

    update setting.py

--- pyAllPay/setting.py
+++ pyAllPay/setting.py
@@ -1,11 +1,14 @@
 __author__ = 'Calvin'
 
 SANDBOX = True
+
 AIO_SERVICE_URL = 'https://payment.allpay.com.tw/Cashier/AioCheckOut'
 AIO_SANDBOX_SERVICE_URL = 'http://payment-stage.allpay.com.tw/Cashier/AioCheckOut'
-MERCHANT_ID = '2000132'
-HASH_KEY = '5294y06JbISpM5x9'
-HASH_IV = 'v77hoKGq4kWxNNIS'
+
+MERCHANT_ID = 'YOUR_MERCHANT_ID' if not SANDBOX else '2000132'
+HASH_KEY = 'YOUR_HASH_KEY' if not SANDBOX else '5294y06JbISpM5x9'
+HASH_IV = 'YOUR_HASH_IV' if not SANDBOX else 'v77hoKGq4kWxNNIS'
+
 # URL Setting.
 RETURN_URL = 'YOUR_RETURN_URL'
 ORDER_RESULT_URL = 'YOUR_ORDER_RESULT_URL'
