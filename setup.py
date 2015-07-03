f0fc3f2cbd496a8e77454dfbf312917a1e768e3c
lockys <locky4567@gmail.com>
2015-07-03 22:01:57 +0800

    des fixed

--- setup.py
+++ setup.py
@@ -7,6 +7,21 @@ __version__ = '0.0.7-dev'
 __author__ = 'Calvin Jeng'
 __email__ = 'lock4567@gmail.com'
 
+long_description = """
+This is allPay(歐付寶) SDK implemented in Python. not All functions are implemented now.
+CheckOutString(), CheckOut(), CheckOutFeedback() has been implemented. In general, it could be used in web developed by Django ..etc
+Features:
+Checkout a payment with following method.
+- CVS
+- ATM
+- WebATM
+- BarCode
+- Credit card
+Dealing with the POST data After the a payment creates or the customer pay the payment.
+Check out github repo: https://github.com/lockys/allPay.py
+"""
+
+
 setup(
     name='pyallpay',
     version=__version__,
@@ -16,7 +31,7 @@ setup(
     url='https://github.com/lockys/allpay.py',
     license='LICENSE',
     description='allPay API in python',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Web Environment',
